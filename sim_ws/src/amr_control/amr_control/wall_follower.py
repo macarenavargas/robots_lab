28f5c986488a06006435a5653158f458270368c7
macarenavargas <macarena.vargashiguero@gmail.com>
2026-02-08 19:22:07 +0000

    wall follower node second aproach

--- sim_ws/src/amr_control/amr_control/wall_follower.py
+++ sim_ws/src/amr_control/amr_control/wall_follower.py
@@ -1,6 +1,11 @@
 from enum import Enum, auto
 import math
+import numpy as np
 
+class State(Enum):
+    FIND_WALL = auto()    
+    FOLLOW_WALL = auto()  
+    ROTATE_CORNER = auto()
 
 class WallFollower:
     """Class to safely explore an environment (without crashing) when the pose is unknown."""
@@ -13,11 +18,7 @@ class WallFollower:
     WHEEL_RADIUS = 0.033  # Radius of the wheels [m]
     WHEEL_SPEED_MAX = LINEAR_SPEED_MAX / WHEEL_RADIUS  # Maximum motor angular speed [rad/s]
 
-    class State(Enum):
-        STOPPED = auto()
-        MOVE_STRAIGHT = auto()
-        TURN = auto()
-
+    
     def __init__(self, dt: float, logger=None, simulation: bool = False) -> None:
         """Wall following class initializer.
 
@@ -31,12 +32,14 @@ class WallFollower:
         self._logger = logger
         self._simulation: bool = simulation
 
-        self._state = self.State.STOPPED
-        self._turn_direction = 0.0
+        self._state = State.FIND_WALL
+       
         self.K_p = 2.0
-        self.K_d = 1.5
-        self._dist_ref = 0.5  # distance that the robot has too keep from the wall
+        self.K_d = 0
         self._prev_error = 0.0  # memory for de derivative term
+
+        self._dist_ref = 0.5  # distance that the robot has too keep from the wall
+        self._vel_ref = 0.15
 
     def compute_commands(self, z_scan: list[float], z_v: float, z_w: float) -> tuple[float, float]:
         """Wall following exploration algorithm.
@@ -54,91 +57,70 @@ class WallFollower:
         """
         # TODO: 2.14. Complete the function body with your code (i.e., compute v and w).
 
-        # scan = [self.SENSOR_RANGE_MAX if math.isinf(d) else d for d in z_scan]
+        z_scan = [r if not math.isnan(r) and not math.isinf(r) else self.SENSOR_RANGE_MAX for r in z_scan]
+       
+        d_front = min(z_scan[-5 :]+z_scan[:5])
+       
+        d_wall = min(z_scan[160 : 200])
 
-        # front_values = scan[-5:] + scan[:5]
-        # left_side_values = scan[55:65]
-        # right_side_values = scan[175:185]
-
-        # valid_front = [v for v in front_values if not math.isnan(v)]
-        # valid_left = [v for v in left_side_values if not math.isnan(v)]
-        # valid_right = [v for v in right_side_values if not math.isnan(v)]
-
-        # min_front = min(valid_front) if len(valid_front) > 0 else 0.0
-
-        # min_left = min(valid_left) if len(valid_left) > 0 else 0.0
-
-        # min_right = min(valid_right) if len(valid_right) > 0 else 0.0
-
-        # 1. Limpieza de Infinitos
-        scan = [self.SENSOR_RANGE_MAX if math.isinf(d) else d for d in z_scan]
-
-        # 2. CÁLCULO DINÁMICO DE ÍNDICES (A prueba de fallos)
-        n = len(scan)  # ¿Son 360? ¿Son 720? Da igual, esto se ajusta solo.
-
-        # En ROS estándar: 0=Frente, n/4=Izq, n/2=Atrás, 3n/4=Derecha
-        idx_front = 0
-        idx_left = int(n * 0.25)  # Aprox 90 grados
-        idx_right = int(n * 0.75)  # Aprox 270 grados
-
-        # Definimos sectores de 10 grados aprox (5 rayos a cada lado)
-        margin = 5
-
-        # Ojo con el slice en Python si idx_right se sale, pero en medio del array es seguro
-        # Frente (cruzando el cero)
-        front_values = scan[-margin:] + scan[:margin]
-        left_side_values = scan[idx_left - margin : idx_left + margin]
-        right_side_values = scan[idx_right - margin : idx_right + margin]
-
-        # 3. Filtrado de NaNs
-        valid_front = [v for v in front_values if not math.isnan(v)]
-        valid_left = [v for v in left_side_values if not math.isnan(v)]
-        valid_right = [v for v in right_side_values if not math.isnan(v)]
-
-        # 4. Mínimos seguros
-        min_front = min(valid_front) if valid_front else 0.0
-        min_left = min(valid_left) if valid_left else 0.0
-        min_right = min(valid_right) if valid_right else 0.0
+        # State machine: criteria for state changes
+        if d_front < 0.2: 
+            self._state = State.ROTATE_CORNER
+        elif d_wall < 1.0: 
+            self._state = State.FOLLOW_WALL
+        else:
+            self._state = State.FIND_WALL
 
         v = 0.0
         w = 0.0
-
-        if self._state == self.State.STOPPED:
+        
+        if self._state == State.ROTATE_CORNER:
             v = 0.0
-            w = 0.0
-            self._state = self.State.MOVE_STRAIGHT
+            w = -0.6  
+            self.prev_error = 0.0 # reset the pd error so it doesnt acummulate
 
-        elif self._state == self.State.MOVE_STRAIGHT:
-            v = 0.15
-            # control PD formula to calculate w
-            distance_to_right_wall = min_right
-            error = self.DIST_REF - distance_to_right_wall
+        elif self._state == State.FOLLOW_WALL:
+           
+            v = self._vel_ref
+            
+            error = self._dist_ref - d_wall
             derivative = (error - self._prev_error) / self._dt
-            w = (self.K_p * error) + (self.K_d * derivative)
-
+    
+            w = (self.K_p * error + self.K_d * derivative)
+            
             self._prev_error = error
 
-            # cinematic restrictions
-            b = self.TRACK / 2
-            w_max_admissible = (self.LINEAR_SPEED_MAX - abs(v)) / b  # maximum value w can take
-            w_filtered = min(w, w_max_admissible)  # in case w exceeds the allowed fisical limit
-            w = max(
-                w_filtered, -w_max_admissible
-            )  # apply the absolute value logic from the formula
+        elif self._state == State.FIND_WALL:
+            # go foward and turn slightly to find the wall
+            v = self._vel_ref
+            w = 0.15 # turn right slightly to look for the waññ
+            self._prev_error = 0.0
 
-            if min_front < 0.45:
-                self._state = self.State.TURN
-                if min_right < min_left:
-                    self._turn_direction = -0.5
-                else:
-                    self._turn_direction = 0.5
+        
+        # cinematic restricction.
+        # dealing with saturation
+        
+        b = self.TRACK / 2.0
+        r = self.WHEEL_RADIUS
+        
 
-        elif self._state == self.State.TURN:
-            v = 0.0
-            w = self._turn_direction
-
-            if min_front > 0.65:
-                self._state = self.State.MOVE_STRAIGHT
-                self._prev_error = 0.0  # reset the error
+        w_limit = (self.WHEEL_SPEED_MAX * r - abs(v)) / b # |w_max| = (|phi_max|*r- v) / b
+        
+        # if estimated w is greater than the limit
+        if abs(w) > w_limit:
+            # we priorize w, and so limit v
+            
+            v_altered = (self.WHEEL_SPEED_MAX * r) - (abs(w) * b) # v_max = |phi_max|*r - |w|*b
+            
+            if v_altered < 0: 
+                # we make sure that the w demand doesn't ask for a negative v
+                v = 0.0
+                # fix w to its rotation maximium (v=0)
+                w = np.sign(w) * (self.WHEEL_SPEED_MAX * r / b) # |w_max| = (|phi_max|*r- 0) / b
+            else:
+                v = v_altered
+        
+        # Log (opcional) para depurar estados
+        self._logger.info(f"State: {self._state.name} | Front: {d_front:.2f} | Wall: {d_wall:.2f} | v: {v:.2f}, w: {w:.2f}")
 
         return v, w
