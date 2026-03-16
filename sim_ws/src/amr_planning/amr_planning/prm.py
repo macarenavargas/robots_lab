39ff2d0051139be900d211c402e9d88c04772b4f
beajimeenez <202300419@alu.comillas.edu>
2026-03-16 11:43:17 +0100

    4.3 finished

--- sim_ws/src/amr_planning/amr_planning/prm.py
+++ sim_ws/src/amr_planning/amr_planning/prm.py
@@ -69,7 +69,16 @@ class PRM:
         )
 
     def _find_closest(self, point) -> tuple[float, float]:
+        """
+            (auxiliar function for the "find_path" method)
+            Finds the closest point on the graph 
 
+            Args : 
+                point : The start or end (x,y)
+
+            Returns: 
+                The closest node that belongs to the graph 
+        """
         smallest_distance = np.inf
         closest_node = None
 
@@ -98,42 +107,81 @@ class PRM:
         if not self._map.contains(goal):
             raise ValueError("Goal location is outside the environment.")
 
-        ancestors: dict[tuple[float, float], tuple[float, float]] = {}  # {(x, y: (x_prev, y_prev)}
-        open_list: dict[tuple[float, float], tuple[float, float]] = {}
-        closed_list = set()
 
-        open_list_empty = False
-        goal_reached = False
+        ancestors: dict[tuple[float, float], tuple[float, float]] = {}  # {(x, y: (x_prev, y_prev)}
 
         # TODO: 4.3. Complete the function body (i.e., replace the code below).
         path: list[tuple[float, float]] = []
 
+        # 1. Get the start and end nodes 
         closest_node_start = self._find_closest(start)
         closest_node_goal = self._find_closest(goal)
 
+
+        # 2. Initilize the open and close list. 
+        open_list: dict[tuple[float, float], tuple[float, float]] = {}
+        closed_list = set()
+
+
+        # 3. Principal loop 
+        # initial node is the start node. add it to the list to initialize the loop 
         g = 0
         h = math.dist(closest_node_start, closest_node_goal)
         f = h + g
         open_list[closest_node_start] = (f, g)
 
-        while not open_list_empty or not goal_reached:
+        goal_reached = False
+
+        while open_list: 
+
+            # extract the node that has the minimum value from the open list 
             node = min(open_list, key=lambda k: open_list.get(k)[0])
 
-            f_node, g_node = open_list[node]
+            #extract their values and delete it from the open list 
+            _ , g_node = open_list[node]
             del open_list[node]
 
+            if node == closest_node_goal:
+                goal_reached = True
+                break
+
+            # extraet their neighbours 
             list_closest_neighbours = self._graph[node]
 
             for neighbour in list_closest_neighbours:
+                # calculate their values 
                 g_now = g_node + 1
                 h_now = math.dist(neighbour, closest_node_goal)
                 f_now = g_now + h_now
+
+                # if its not in either lists, add it to the open list 
                 if neighbour not in closed_list and neighbour not in open_list:
                     open_list[neighbour] = (f_now, g_now)
+                    ancestors[neighbour] = node
+                
+                # if it was already in the open list, only add it if it has less g
                 elif neighbour in open_list:
                     if open_list[neighbour][1] > g_now:
                         open_list[neighbour] = (f_now, g_now)
+                        ancestors[neighbour] = node
+
+            
+
+            # add the original node to the close list 
             closed_list.add(node)
+
+        # if we have not got to the goal node, cannot compute the path 
+        if not goal_reached:
+            raise RuntimeError("No path found")
+
+        # add the real starting and ending points 
+        if start != closest_node_start:
+            ancestors[closest_node_start] = start
+        if goal != closest_node_goal:
+            ancestors[goal] = closest_node_goal
+        
+        # reconstruct the path : 
+        path = self._reconstruct_path(closest_node_start, closest_node_goal, ancestors)
 
         return path
 
